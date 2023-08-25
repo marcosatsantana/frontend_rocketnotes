498c074e1c8e94499535b2de35dd2bea53579b9c
marcosxk122 <marcost.santana@hotmail.com>
2023-08-25 10:30:43 -0300

    migrate TS to JS to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import GlobalStyles from './styles/global'
 import { AuthProvider } from './hooks/auth';
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
